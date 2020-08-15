2e70c98a9b6c613181acd9c4adb416fc79a884e5
Adi.Peretz <adiptz@gmail.com>
2020-08-15 22:23:15 +0300

    Updates

---
+++ precache-manifest.f2fa306f2e2c1b07505c840266db446a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4bb85dbffb5eb20a39a2f0aa268607",
+    "url": "/Rewire/index.html"
+  },
+  {
+    "revision": "7d09f56ccfedaf0e8791",
+    "url": "/Rewire/static/css/main.5f16e1b4.chunk.css"
+  },
+  {
+    "revision": "14584cfca11d8862f4a6",
+    "url": "/Rewire/static/js/2.8fb43b19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rewire/static/js/2.8fb43b19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d09f56ccfedaf0e8791",
+    "url": "/Rewire/static/js/main.08c8237b.chunk.js"
+  },
+  {
+    "revision": "74a34aff1e0be7eadc62",
+    "url": "/Rewire/static/js/runtime-main.1297e179.js"
+  }
+]);
